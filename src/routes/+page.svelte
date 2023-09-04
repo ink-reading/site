4d9bfd633bc2d24ec466d50d709b94e8e3ed8f5e
Jianan Sun <isunjn@gmail.com>
2023-09-05 02:13:26 +0800

    feat: 'under construction'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>ink</title>
+</svelte:head>
+
+<main class="h-screen flex flex-col justify-center items-center text-[#ea6c61] font-mono">
+  <div class="grow flex items-center">
+    <div class="flex flex-col justify-center items-baseline gap-6 sm:flex-row">
+      <div class="text-7xl">ink</div>
+      <div class="text-xl">/// UNDER CONSTRUCTION</div>
+    </div>
+  </div>
+  <div class="my-2 text-sm text-gray-400">https://ink-reading.app</div>
+  <div class="mb-8 text-sm text-gray-400">https://github.com/ink-reading/ink</div>
+</main>
+
+<style>
+  main::before {
+    content: "";
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    z-index: -1;
+    background: radial-gradient(circle,rgba(2,0,36,0) 0,#fcfcfc 100%);
+  }
+  main::after {
+    content: "";
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    z-index: -2;
+    background-image: url(/grid.svg);
+    filter: invert(1);
+    opacity: .5;
+  }
+</style>
